c0cb07183cad74fc16f7d18993e412e1f740bee2
unknown <syedaliaskarizaidi1@gmail.com>
2025-12-19 12:31:00 +0500

    Updated Successfully😍

--- fullstack/frontend-book/docusaurus.config.js
+++ fullstack/frontend-book/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ai-spec-driven-book-six.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -94,6 +94,10 @@ const config = {
             sidebarId: 'tutorialSidebar',
             position: 'left',
             label: 'Textbook',
+          },
+          {
+            type: 'search',
+            position: 'right',
           },
           {
             href: 'https://github.com/AliAskariGithub',
